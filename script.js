2c2dfbcdadbcc77485743a0c13b34885470d9323
Max <maxmawnstor@gmail.com>
2024-03-16 21:08:51 -0500

    files added

---
+++ script.js
@@ -0,0 +1,15 @@
+const timerDisplay = document.getElementById("timer");
+const startButton = document.getElementById("start");
+const startScreen = document.getElementById("start-screen");
+const questionsContainer = document.getElementById("questions");
+const questionText = document.getElementById("question-words");
+const optionsContainer = document.getElementById("options");
+const endScreen = document.getElementById("quiz-end");
+const scoreDisplay = document.getElementById("score-final");
+const nameInput = document.getElementById("initials");
+const submitButton = document.getElementById("submit-score");
+
+
+
+
+
